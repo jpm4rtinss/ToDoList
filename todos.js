399a3a59913ba3066d905adfd951844cd8eb8bbb
João Pedro Martins <joaopedromsn@gmail.com>
2020-08-27 18:39:20 -0300

    delete element

--- todos.js
+++ todos.js
@@ -8,20 +8,52 @@ var toDos = [
 ]
 
 function renderToDos(){
+    listElement.innerHTML = ''
+
     for (todo of toDos) {
-    //create li element
+    //create elements - "li" and "a" 
     var toDoElement = document.createElement('li')
+    
+    var linkElement = document.createElement('a')
+    var linkText = document.createTextNode('Excluir')
+    linkElement.appendChild(linkText)
+    linkElement.setAttribute('href', '#')
+    
+    //saving the position of each element in array
+    var position = toDos.indexOf(todo)
+
+    //call the function to delete when the click on element "a" happens
+    linkElement.setAttribute('onclick', 'deleteToDo(' + position + ')')
+
+
     //get the texts of array
     var toDoText = document.createTextNode(todo)
-    //add texts to the li and the li to the ul
+
+    //add texts and element "a" to "li" and the "li" to "ul"
     toDoElement.appendChild(toDoText)
+    toDoElement.appendChild(linkElement)
     listElement.appendChild(toDoElement)
 
     
     }
 }
+
 renderToDos()
 
 function addToDo (){
-    var toDoText = 
+    //get the value of the input and add to the array 
+    var toDoText = inputElement.value
+    toDos.push(toDoText)
+    
+    //reset input
+    inputElement.value = ''
+    renderToDos()
+}
+
+buttonElement.onclick = addToDo
+
+function deleteToDo(position){
+    toDos.splice(position, 1)
+    renderToDos()
+
 }
